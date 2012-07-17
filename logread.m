f4b4300dd153b4d8c25e09829aa43ac40000f667
Shaunak Kishore <skishore@mit.edu>
2012-07-17 15:27:47 -0700

    Revert logread

--- logread.m
+++ logread.m
@@ -1,22 +1,19 @@
 %% read in file
-fin = fopen('log.txt','r');
+fin = fopen('strippedlog.txt','r');
 while ~feof(fid)
-    status = fscanf(fin,'Log type %d:',1);
-    skip = fscanf(fin,'%s',1);
+    status = fscanf(fin,'%d',1);
     if isempty(status)
         break
     end
     fprintf(1,'status: %d\n',status);
     if status == 0 || status == 1
-        N=fscanf(fin,'N: %d',1);
-        D=fscanf(fin,', D: %d',1);
-        K=fscanf(fin,', K: %d',1);
+        N=fscanf(fin,'%d',1);
+        D=fscanf(fin,'%d',1);
+        K=fscanf(fin,'%d',1);
         fprintf(1,'params: %d %d %d\n',N,D,K);
         mu = zeros(K,D);
         sigma = zeros(K,D,D);
         for k=1:K
-            skip=fscanf(fin,'Cluster %d mu and sigma:',1);
-            mu(k,:)=fscanf(fin,'%f',D);
             mu(k,:)=fscanf(fin,'%f',D);
             sigma(k,:,:)=fscanf(fin,'%f',[D D]);
             disp('mu:');
@@ -33,16 +30,14 @@ while ~feof(fid)
         
     end
     if status == 1
-        skip = fscanf(fin,'Assignments of %d points:',1);
         assignments = fscanf(fin,'%d',N);
         disp('assignments:');
         disp(assignments);
     end
     if status == 2
-        N=fscanf(fin,'Width: %d',1);
-        D=fscanf(fin,', height: %d',1);
+        N=fscanf(fin,'%d',1);
+        D=fscanf(fin,'%d',1);
         fprintf(1,'params: %d %d\n',N,D);
-        skip=fscanf(fin,'Data:',1)';
         data=fscanf(fin,'%f',[D N])';
         disp('data:');
         disp(data);
