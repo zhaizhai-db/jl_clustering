801bd0299bdcedfbf50678541969a02912140eb2
Shaunak Kishore <skishore@mit.edu>
2012-07-19 20:33:42 -0700

    Segregated 32x32 test from other sets

--- data/32x32/convert_data.py
+++ data/32x32/convert_data.py
@@ -4,9 +4,8 @@ import sys
 import os
 from random import randint, sample
 
-DATA_RATIOS = {'holdout': 0.33, 'test': 0.33}
 DOWNSAMPLE_RATE_N = 0.4
-DOWNSAMPLE_RATE_D = 0.1
+DOWNSAMPLE_RATE_D = 0.25
 DOWNSAMPLE = True
 
 def dot(v, u):
@@ -32,67 +31,46 @@ def downsample(clusters, rate_n, rate_d):
         new_clusters[c] = [proj(x) for x in sample(clusters[c], new_n)]
     return new_clusters
 
-dataset_name = '32x32'
-clusters = collections.defaultdict(list)
-row_len = None
+for data_type in ('pretrain', 'holdout', 'test'):
+    dataset_name = '32x32'
+    clusters = collections.defaultdict(list)
+    row_len = None
 
-img_list = open('imgs.txt')
-for line in img_list:
-    l = line.strip()
-    if not l:
-        continue
+    img_list = open('%s.txt' % (data_type,))
+    for line in img_list:
+        l = line.strip()
+        if not l:
+            continue
 
-    c = l.split('_')[0].lower()
-    img_path = os.path.join('jpgs', l)
-    row = list(Image.open(img_path).getdata())
-    clusters[c].append(row)
+        c = l.split('_')[0].lower()
+        img_path = os.path.join('jpgs', l)
+        row = list(Image.open(img_path).getdata())
+        clusters[c].append(row)
 
-    if row_len is None:
-        row_len = len(row)
-    assert row_len == len(row)
+        if row_len is None:
+            row_len = len(row)
+        assert row_len == len(row)
 
-class_names = list(clusters)
-f = open(dataset_name + '_ids.txt', 'w')
-for i, c in enumerate(class_names):
-    f.write('%d: %s\n' % (i, c))
-f.close()
-
-if DOWNSAMPLE:
-    print 'Downsampling...'
-    clusters = downsample(clusters, DOWNSAMPLE_RATE_N, DOWNSAMPLE_RATE_D)
-    print 'Done downsampling'
-
-data_slices = collections.defaultdict(list)
-
-for i, c in enumerate(class_names):
-    data = [row + [i] for row in clusters[c]]
-    n = len(data)
-
-    for s in DATA_RATIOS:
-        m = int(DATA_RATIOS[s] * n)
-        data_slices[s].extend(data[:m])
-        data = data[m:]
-
-    data_slices['pretrain'].extend(data)
-
-
-for s in data_slices:
-    print s, len(data_slices[s])
-
-
-def print_to_file(slice_name, data_slice):
-    file_name = dataset_name + '.' + slice_name
-
-    N = len(data_slice)
-    K = len(clusters)
-    D = len(clusters.values()[0][0])
-
-    f = open('../' + file_name, 'w')
-    f.write('%d %d %d\n' % (N, D, K))
-    for row in data_slice:
-        f.write(' '.join(str(x) for x in row) + '\n')
+    class_names = list(clusters)
+    f = open(dataset_name + 'ids.txt', 'w')
+    for i, c in enumerate(class_names):
+        f.write('%d: %s\n' % (i, c))
     f.close()
 
-for s in data_slices:
-    print_to_file(s, data_slices[s])
+    if DOWNSAMPLE:
+        print 'Downsampling %s...' % (data_type,)
+        clusters = downsample(clusters, DOWNSAMPLE_RATE_N, DOWNSAMPLE_RATE_D)
 
+    print 'Dumping %s to file...' % (data_type,)
+    data = sum(clusters.values(), [])
+    N = len(data)
+    D = len(data[0])
+    K = len(clusters)
+
+    file_name = dataset_name + '.' + data_type
+    f = open('../' + file_name, 'w')
+    f.write('%d %d %d\n' % (N, D, K))
+    for row in data:
+        f.write(' '.join(str(x) for x in row) + '\n')
+    f.close()
+    print 'Done with %s.' % (data_type,)
